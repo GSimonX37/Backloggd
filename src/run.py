adb510dc21bc93ddf868031595736abd1bc27394
GSimonX37 <GSimonX37@gmail.com>
2024-03-24 13:59:19 +0700

    Implement an application launcher
    Implement a separate application launcher in a Docker container

---
+++ src/run.py
@@ -0,0 +1,30 @@
+import sys
+import uvicorn
+
+from app.application import model
+
+
+def main() -> None:
+    """
+    Тока входа запуска приложения в контейнере Docker;
+
+    :return: None.
+    """
+
+    directory = sys.argv[1]
+
+    file = rf'/models/{directory}/{directory}.joblib'
+    labels = rf'/models/{directory}/labels.json'
+
+    model.load(file, labels)
+
+    uvicorn.run(
+        app="app.application:app",
+        host="0.0.0.0",
+        port=8000,
+        log_level="info"
+    )
+
+
+if __name__ == "__main__":
+    main()
