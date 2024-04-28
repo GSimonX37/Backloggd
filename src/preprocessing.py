0b0b8823672dbcf98ec0d398096aea6c77b06e97
GSimonX37 <GSimonX37@gmail.com>
2024-04-28 20:18:44 +0700

    Optimize text preprocessing
    Move unnecessary transformations outside the function to use it in your
    application

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,6 +8,14 @@ from config.paths import PATH_RAW_DATA
 from config.paths import PATH_PREPROCESSED_DATA
 
 
+def insert(key: int, values: pd.Series) -> list:
+    if key in values.index:
+        value = values[key]
+        return [value] if isinstance(value, str) else value.to_list()
+    else:
+        return []
+
+
 def main():
     """
     Тока входа предварительной обработки данных;
@@ -23,10 +31,38 @@ def main():
         games = pd.read_csv(f'{PATH_RAW_DATA}/{name}/games.csv')
         genres = pd.read_csv(f'{PATH_RAW_DATA}/{name}/genres.csv')
 
-        data = preprocessing(
-            games=games,
-            genres=genres
+        games = games.copy()
+        genres = genres.copy()
+
+        # Удаление явных дубликатов.
+        games = games.drop_duplicates()
+        genres = genres.drop_duplicates()
+
+        # Удаление значения "indie" из поля "genres"
+        genres = genres[genres['genre'] != 'Indie']
+
+        # Объединение данных.
+        values = genres['genre'].value_counts().index[:20]
+        genres = (genres
+                  .loc[genres['genre'].isin(values), :]
+                  .set_index('id'))['genre']
+
+        games.insert(
+                loc=games.shape[1],
+                column='genres',
+                value=games['id'].apply(insert, values=genres)
         )
+        data = games[['description', 'genres']]
+
+        # Удаление данных без целевой переменной.
+        data = data.loc[data['genres'].map(bool), :]
+
+        data = data.dropna()
+
+        data['description'] = preprocessing(data['description'])
+
+        # Удаление записей, с длиной текста меньше 50 символов.
+        data = data[data['description'].str.len() >= 50]
 
         # Сохранение предобработанных данных.
         data.to_csv(
--- src/utils/data/preprocessing/preprocessing.py
+++ src/utils/data/preprocessing/preprocessing.py
@@ -6,55 +6,16 @@ from .preparation import letters
 from .lemmatization import lemmatize
 
 
-def insert(key: int, values: pd.Series) -> list:
-    if key in values.index:
-        value = values[key]
-        return [value] if isinstance(value, str) else value.to_list()
-    else:
-        return []
-
-
-def preprocessing(games: pd.DataFrame, genres: pd.DataFrame) -> pd.DataFrame:
-    games = games.copy()
-    genres = genres.copy()
-
-    # Удаление явных дубликатов.
-    games = games.drop_duplicates()
-    genres = genres.drop_duplicates()
-
-    # Удаление значения "indie" из поля "genres"
-    genres = genres[genres['genre'] != 'Indie']
-
-    # Объединение данных.
-    values = genres['genre'].value_counts().index[:20]
-    genres = (genres
-              .loc[genres['genre'].isin(values), :]
-              .set_index('id'))['genre']
-
-    games.insert(
-        loc=games.shape[1],
-        column='genres',
-        value=games['id'].apply(insert, values=genres)
-    )
-    data = games[['description', 'genres']]
-
-    # Удаление данных без целевой переменной.
-    data = data.loc[data['genres'].map(bool), :]
-
-    data = data.dropna()
-
-    # Предварительная подготовка текста
-    data['description'] = data['description'].apply(letters)
-    data['description'] = data['description'].apply(length, size=2)
-    data['description'] = data['description'].apply(spaces)
+def preprocessing(data: pd.Series) -> pd.Series:
+    # Очистка текста
+    data = data.apply(letters)
+    data = data.apply(length, size=2)
+    data = data.apply(spaces)
 
     # Лемматизация текста.
-    data['description'] = data['description'].apply(lemmatize)
-
-    # Удаление записей, с длиной текста меньше 50 символов.
-    data = data[data['description'].str.len() >= 50]
+    data = data.apply(lemmatize)
 
     # Приведение текста к нижнему регистру.
-    data['description'] = data['description'].str.lower()
+    data = data.str.lower()
 
     return data
