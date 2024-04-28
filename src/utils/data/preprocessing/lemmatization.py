2862b2c05c342a934ae097be69fdc78e00fbe177
GSimonX37 <GSimonX37@gmail.com>
2024-04-28 20:16:19 +0700

    Optimize text lemmatization
    Add hashing function to increase speed

--- src/utils/data/preprocessing/lemmatization.py
+++ src/utils/data/preprocessing/lemmatization.py
@@ -2,11 +2,12 @@ from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 from nltk.corpus import wordnet
 
+from functools import cache
 
 lemmatizer = WordNetLemmatizer()
-words = {}
 
 
+@cache
 def get_tag(word):
     tag = pos_tag([word])[0][1][0].upper()
     tag_dict = {"J": wordnet.ADJ,
@@ -15,6 +16,11 @@ def get_tag(word):
                 "R": wordnet.ADV}
 
     return tag_dict.get(tag, wordnet.NOUN)
+
+
+@cache
+def lemma(word: str, tag) -> str:
+    return lemmatizer.lemmatize(word, tag)
 
 
 def lemmatize(text: str) -> str:
@@ -28,13 +34,9 @@ def lemmatize(text: str) -> str:
     result = []
 
     for word in text.split():
-        if lemma := words.get(word):
-            result.append(lemma)
-        else:
-            tag = get_tag(word)
-            lemma = lemmatizer.lemmatize(word, tag)
-            result.append(lemma)
-            words[text] = lemma
+        tag = get_tag(word)
+        word = lemma(word, tag)
+        result.append(word)
 
     result = ' '.join(result)
 
