672b8408e71584bfa846cf1841f7cbfad9cb380e
GSimonX37 <GSimonX37@gmail.com>
2024-04-28 19:06:19 +0700

    Fix lemmatization of the text

--- src/utils/data/preprocessing/lemmatization.py
+++ src/utils/data/preprocessing/lemmatization.py
@@ -1,9 +1,20 @@
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
+from nltk.corpus import wordnet
 
 
 lemmatizer = WordNetLemmatizer()
 words = {}
+
+
+def get_tag(word):
+    tag = pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+
+    return tag_dict.get(tag, wordnet.NOUN)
 
 
 def lemmatize(text: str) -> str:
@@ -20,7 +31,7 @@ def lemmatize(text: str) -> str:
         if lemma := words.get(word):
             result.append(lemma)
         else:
-            tag = pos_tag([word])[0][1][0].lower()
+            tag = get_tag(word)
             lemma = lemmatizer.lemmatize(word, tag)
             result.append(lemma)
             words[text] = lemma
