2eee774585ebc0d3b0355ca0b7566ce7cce02831
GSimonX37 <GSimonX37@gmail.com>
2024-04-28 12:02:27 +0700

    Add tests for data preprocessing functions

---
+++ tests/utils/data/preprocessing/test_lemmatization.py
@@ -0,0 +1,16 @@
+import pytest
+
+from utils.data.preprocessing.lemmatization import lemmatize
+
+
+@pytest.mark.parametrize(
+    ['text', 'expected'],
+    [
+        ('', ''),
+        ('be is are', 'be be be'),
+        ('cats dogs elephants mice', 'cat dog elephant mouse')
+    ]
+)
+def test_lemmatize(text: str, expected: str):
+    result = lemmatize(text)
+    assert result == expected
---
+++ tests/utils/data/preprocessing/test_preparation.py
@@ -0,0 +1,50 @@
+import pytest
+
+from utils.data.preprocessing.preparation import spaces
+from utils.data.preprocessing.preparation import length
+from utils.data.preprocessing.preparation import letters
+
+
+@pytest.mark.parametrize(
+    ['text', 'expected'],
+    [
+        ('', ''),
+        ('One. Two. Three. Four. Five.', 'One  Two  Three  Four  Five '),
+        ('One - 1. Two - 2. Three - 3.', 'One      Two      Three     '),
+        ('1, 2, 3, 4, 5, 6, 7, 8, 9, 0.', '                             ')
+    ]
+)
+def test_letters(text: str, expected: str):
+    result = letters(text)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    ['text', 'expected'],
+    [
+        ('', ''),
+        (' ', ' '),
+        ('   ', ' '),
+        ('\n\n\n', ' '),
+        ('string', 'string'),
+        ('string string', 'string string'),
+        (' string ', ' string '),
+        ('string\nstring\nstring\nstring', 'string string string string')
+    ]
+)
+def test_spaces(text: str, expected: str):
+    result = spaces(text)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    ['text', 'size', 'expected'],
+    [
+        ('', 2, ''),
+        (' ', 2, ' '),
+        ('Example of a small sentence', 2, 'Example     small sentence'),
+    ]
+)
+def test_length(text: str, size: int, expected: str):
+    result = length(text, size)
+    assert result == expected
